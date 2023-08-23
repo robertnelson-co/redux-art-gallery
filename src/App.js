a356cbf141a8b6f6222f876d8602a7a5be7cc6b7
robertnelson-co <hello@robertnelson.co>
2023-08-23 14:50:54 -0400

    reworking App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import {React, useState, useEffect} from 'react';
-import logo from './logo.svg';
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice';
 import Gallery from './gallery';
 import ButtonBar from './buttonbar';
 import './App.css';
 
-function App() {
-  let [artId, setArtId] = useState(12045)
-  let [data, setData] = useState({})
+function App(props) {
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.data)
 
-  useEffect(() => {
-    document.title="Welcome to Bob's Art Gallery"
-    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
-    .then (response => response.json())
-    .then (resData => setData(resData))
-}, [artId])
-
-  return (
-    <div className="App">
-      <Gallery />
-      <ButtonBar />
-    </div>
-  );
+  const renderImg = () => {
+    if (data.apiData) {
+      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>There's supposed to be something here...</p>
+    }
+  }
 }
 
-export default App;
+useEffect(() => {
+  dispatch(fetchData())
+}, [props.objectId], dispatch)
+
+return (
+  <div className="App">
+    <div>
+      <button onClick={() => dispatch(fetchData())}>Huh?</button>
+      <button onClick={() => dispatch(clearData())}>Clear</button>
+      <button onClick={() => dispatch(incrementId())}>Next</button>
+      <button onClick={() => dispatch(decrementId())}>Back</button>
+    </div>
+    <input value={ data.objectId } onChange={(e) => {
+      dispatch(inputId(Number(e.target.value)))
+    }} />
+    <div>
+      {data.objectId}
+      {renderImg()}
+    </div>
+  </div>
+)
+
+//function App() {
+//  let [artId, setArtId] = useState(12045)
+//  let [data, setData] = useState({})
+//
+//  useEffect(() => {
+//    document.title="Welcome to Bob's Art Gallery"
+//    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+//    .then (response => response.json())
+//    .then (resData => setData(resData))
+//}, [artId])
+//
+//  return (
+//    <div className="App">
+//      <Gallery />
+//      <ButtonBar />
+//    </div>
+//  );
+//}
+
+
